4c4e6254e3b4c3d5105932f8fc89633d3a9e6d56
Joel Wang <joel.wang@isipl.local>
2019-01-22 17:09:28 +1100

    push build folder

---
+++ my-app/build/precache-manifest.1e92fd1891a8ec2b7386c4a9aae118d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "061098204563725ffe7b",
+    "url": "/static/js/main.06109820.chunk.js"
+  },
+  {
+    "revision": "8e1e42867782fa9b3e88",
+    "url": "/static/js/1.8e1e4286.chunk.js"
+  },
+  {
+    "revision": "061098204563725ffe7b",
+    "url": "/static/css/main.9ac7f61a.chunk.css"
+  },
+  {
+    "revision": "0586f73458e00b02b57d8ddd7d95200c",
+    "url": "/index.html"
+  }
+];
